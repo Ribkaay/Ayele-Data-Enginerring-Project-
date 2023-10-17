8dfd1bedb605c7405ee9a4c2c38ec575352339c6
Ribkaay <ribkaay@gmail.com>
2023-10-17 14:15:47 -0700

    LOAN Application dataset
    Request acces from a REST API by sending an HTTP request and processing the response

--- CreditCardSystem.py
+++ CreditCardSystem.py
@@ -8,6 +8,7 @@ Date: 10/14/2023
 import os
 import sys
 import pyspark
+import requests
 import pandas as pd 
 import random
 import secret
@@ -95,7 +96,7 @@ def transform_cdw_sapp_custmer (df_cdw_sapp_custmer):
                                   ).withColumn("CUST_CITY", col("CUST_CITY").cast(StringType()) 
                                   ).withColumn("CUST_STATE", col("CUST_STATE").cast(StringType())
                                   ).withColumn("CUST_COUNTRY", col("CUST_COUNTRY").cast(StringType()) 
-                                  ).withColumn("CUST_ZIP", col("CUST_ZIP").cast(IntegerType())    
+                                  ).withColumn("CUST_ZIP", col("CUST_ZIP").cast(StringType())    
                                   ).withColumn("CUST_PHONE", col("CUST_PHONE").cast(StringType())
                                   ).withColumn("CUST_EMAIL", col("CUST_EMAIL").cast(StringType()) 
                                   ).withColumn("LAST_UPDATED", col("LAST_UPDATED").cast(TimestampType()))
@@ -134,7 +135,7 @@ def transform_cdw_sapp_branch(df_cdw_sapp_branch):
                                   ).withColumn('BRANCH_STREET', col("BRANCH_STREET").cast(StringType())
                                   ).withColumn('BRANCH_CITY', col("BRANCH_CITY").cast(StringType()) 
                                   ).withColumn('BRANCH_STATE', col("BRANCH_STATE").cast(StringType())
-                                  ).withColumn('BRANCH_ZIP', col("BRANCH_ZIP").cast(IntegerType())
+                                  ).withColumn('BRANCH_ZIP', col("BRANCH_ZIP").cast(StringType())
                                   ).withColumn('BRANCH_PHONE', col("BRANCH_PHONE").cast(StringType())
                                   ).withColumn("LAST_UPDATED", col("LAST_UPDATED").cast(TimestampType()))  
   cdw_sapp_branch.show()
@@ -209,6 +210,62 @@ def load_to_mysql(dataframe,table_name):
 
 
 
+
+""" 4. Functional Requirements - LOAN Application Dataset  
+ On this part of project I used  LOAN Application. First https://raw.githubusercontent.com/platformps/LoanDataset/main/loan_data.json 
+ request acces from a REST API by sending an HTTP request and processing the response. After got the respond load it to pyspark and load it on database. 
+ Finally create o analyze and visualize the data.
+"""
+
+
+def Loan_Application_dataset():
+    spark = SparkSession.builder.appName('Loan Application dataset').getOrCreate()
+
+    # Create a Python program to GET (consume) data from the above API
+    # endpoint for the loan application dataset.
+
+    url = "https://raw.githubusercontent.com/platformps/LoanDataset/main/loan_data.json"
+
+    #Request the API endpoint
+    response = requests.get(url)
+    data_loan = response.json()  #get Json data from the response
+
+    """Find the status code of the above API endpoint.
+    Hint: status code could be 200, 400, 404, 401"""
+
+    print(f"The request status code is: {response.status_code}" )  # print response.status_code
+
+    """Once Python reads data from the API, utilize PySpark to load data into
+    RDBMS (SQL). The table name should be CDW-SAPP_loan_application
+    in the database.Note: Use the “creditcard_capstone” database."""
+
+    #Load in to spark dataframe
+    data_loan_df = spark.createDataFrame(data_loan)
+
+    # #Define  properties to connect to database
+    mysql_properties = {
+        "user":  secret.mysql_username,
+    "password" :  secret.mysql_password,
+    "drive": "com.msql.cj.jdbc.Driver",
+    }
+
+
+    table_name = "`CDW-SAPP_loan_application`"
+
+    data_loan_df.write.format("jdbc") \
+        .mode("overwrite") \
+        .option("url", "jdbc:mysql://localhost:3306/creditcard_capstone") \
+        .option("dbtable", table_name) \
+        .option("user", secret.mysql_username) \
+        .option("password",secret.mysql_password) \
+        .save()
+
+    data_loan_df.show()
+    spark.stop()
+
+
+
+
    
 
 
@@ -216,22 +273,28 @@ def main():
 
   spark = create_spark_session('Credit Card System')
   #Extract JSON 
-  # df_cdw_sapp_custmer = extract_cdw_sapp_custmer(spark)
+  df_cdw_sapp_custmer = extract_cdw_sapp_custmer(spark)
   df_cdw_sapp_branch = extract_cdw_sapp_branch (spark)
-  # df_cdw_sapp_credit= extract_cdw_sapp_credit(spark) 
+  df_cdw_sapp_credit= extract_cdw_sapp_credit(spark) 
 
   #Transform to dataframe 
-  # cdw_sapp_custmer = transform_cdw_sapp_custmer (df_cdw_sapp_custmer)
+  cdw_sapp_custmer = transform_cdw_sapp_custmer (df_cdw_sapp_custmer)
   cdw_sapp_branch= transform_cdw_sapp_branch(df_cdw_sapp_branch)
-  # cdw_sapp_credit = transform_cdw_sapp_credit(df_cdw_sapp_credit)
+  cdw_sapp_credit = transform_cdw_sapp_credit(df_cdw_sapp_credit)
 
   #Load to Database
 
   db_connection()
   
-  # cdw_sapp_custmer = load_to_mysql(cdw_sapp_custmer,"cdw_sapp_custmer")
+  cdw_sapp_custmer = load_to_mysql(cdw_sapp_custmer,"cdw_sapp_custmer")
   cdw_sapp_branch = load_to_mysql(cdw_sapp_branch,"cdw_sapp_branch" )
-  # cdw_sapp_credit = load_to_mysql(cdw_sapp_credit, "cdw_sapp_credit")
+  cdw_sapp_credit = load_to_mysql(cdw_sapp_credit, "cdw_sapp_credit")
+
+   #request acces from a REST API and load the respond on database
+  Loan_Application_dataset()
+  
+
+
 if __name__=="__main__":
   main()
   
